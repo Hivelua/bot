196896cea7da93114c7b3628b0a4a71c3fe565b0
¿zey <adiciurila1999@gmail.com>
2026-04-19 04:08:04 +0300

    Update index.js

--- index.js
+++ index.js
@@ -24,10 +24,7 @@ let warns = new Map();
 
 // Auto-warn action configuration
 const warnActions = {
-    3: { action: 'mute', duration: '1h', deleteWarns: true },
-    5: { action: 'mute', duration: '1d', deleteWarns: true },
-    6: { action: 'demote', deleteWarns: true },
-    7: { action: 'ban', deleteWarns: true }
+    5: { action: 'demote', deleteWarns: true },
 };
 
 // Load warnings from file
